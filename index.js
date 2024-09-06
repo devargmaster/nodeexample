a79d31e9d77c743e0002b490011dd541b2eb9116
WalterArce <walter.arce@davinci.edu.ar>
2024-09-06 18:26:48 -0300

    configuracion para heroku

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const express = require('express');
 const routerApi = require('./routes');
 const { logErrors, errorHandler, boomErrorHandler } = require('./middlewares/error.handler');
+const cors = require('cors');
 const app = express();
 const port = 3000;
 
 app.use(express.json());
+const whitelist = ['http://localhost:8000','https://myapp.co'];
+const options = {
+  origin : (origin, callback) => {
+    if (whitelist.includes(origin) || !origin)
+    {
+      callback(null,true)
+    }
+    else {
+      callback(new Error('No permitido'));
+    }
+  }
+}
+
+app.use(cors());
 
 routerApi(app);
 app.use(logErrors);
