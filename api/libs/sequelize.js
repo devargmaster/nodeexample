e752670522e971be273f90f63ba7d9d8975c5b68
WalterArce <walter.arce@davinci.edu.ar>
2024-09-10 12:13:34 -0300

    agrego el modelo para usar con sequelize

--- api/libs/sequelize.js
+++ api/libs/sequelize.js
@@ -1,5 +1,5 @@
-const {Sequelize} = require('sequelize');
-const {config} = require('../config/config');
+const { Sequelize } = require('sequelize');
+const { config } = require('../config/config');
 const USER = encodeURIComponent(process.env.DB_USER);
 const PASSWORD = encodeURIComponent(process.env.DB_PASSWORD);
 const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
@@ -7,6 +7,6 @@ const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${
 const sequelize = new Sequelize(URI,
     {
         dialect: 'postgres',
-        logging:console.log,});
-
+        logging: console.log,
+    });
 module.exports = sequelize;
