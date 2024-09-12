b6aa5d39ed7db1281c13e402c4e0606bc9a16581
WalterArce <walter.arce@davinci.edu.ar>
2024-09-12 20:22:11 -0300

    otro fix por cors

--- api/index.js
+++ api/index.js
@@ -1,37 +1,47 @@
 const express = require('express');
-const routerApi = require('./routes');
-const swaggerDocs = require('./swagger');
-const { logErrors, errorHandler, boomErrorHandler } = require('./middlewares/error.handler');
 const cors = require('cors');
+const routerApi = require('./routes');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger');
+const { logErrors, boomErrorHandler, errorHandler } = require('./middlewares/error.handler');
+
 const app = express();
 const port = process.env.PORT || 3000;
-// Integracion de  Swagger
 
+// Middleware para parsear JSON
 app.use(express.json());
-const whitelist = ['http://localhost:8000','https://hitflow.com.ar'];
-const options = {
-  origin : (origin, callback) => {
-    if (whitelist.includes(origin) || !origin)
-    {
-      callback(null,true)
-    }
-    else {
-      callback(new Error('No permitido'));
+
+// Configuración de CORS
+const whitelist = [
+  'http://localhost:8000',
+  'https://hitflow.com.ar',
+  'https://whispering-brook-12889-24774d694fdb.herokuapp.com'
+];
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (whitelist.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('No permitido por CORS'));
     }
   }
-}
+};
 
-app.use(cors());
-app.get('/api',(req,res)=>{
-  res.send('Server Express HitFlow');
-});
+app.use(cors(corsOptions));
 
+// Configurar Rutas
 routerApi(app);
+
+// Middlewares de manejo de errores
 app.use(logErrors);
 app.use(boomErrorHandler);
 app.use(errorHandler);
 
-swaggerDocs(app, port);
+// Integración de Swagger
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
+  console.log(`Swagger docs available at http://localhost:${port}/api-docs`);
 });
