5a1f39bb8aac4ee9c22a95dbdb755ef82938db04
WalterArce <walter.arce@davinci.edu.ar>
2024-09-12 18:45:14 -0300

    configuracion para cors

--- api/index.js
+++ api/index.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const routerApi = require('./routes');
 const swaggerDocs = require('./swagger');
+const swaggerUi = require('swagger-ui-express');
 const { logErrors, errorHandler, boomErrorHandler } = require('./middlewares/error.handler');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
 // Integracion de  Swagger
 
-app.use(express.json());
+
 const whitelist = ['http://localhost:8000','https://hitflow.com.ar','https://whispering-brook-12889-24774d694fdb.herokuapp.com'];
 const options = {
   origin : (origin, callback) => {
@@ -33,12 +34,20 @@ app.use(cors({
   }
 }));
 
+app.use(express.json()); // Middleware para parsear JSON
+
+// Configurar Swagger
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
+// Configurar Rutas
 routerApi(app);
+
+// Middlewares de manejo de errores
 app.use(logErrors);
 app.use(boomErrorHandler);
 app.use(errorHandler);
 
-swaggerDocs(app, port);
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
+  console.log(`Swagger docs available at http://localhost:${port}/api-docs`);
 });
