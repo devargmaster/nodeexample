38ef5e8cd9f5451833f3558670172d2635d4c63f
WalterArce <walter.arce@davinci.edu.ar>
2024-09-12 18:26:37 -0300

    agrego configuracion de entorno para heroku si es production o no

--- api/libs/sequelize.js
+++ api/libs/sequelize.js
@@ -5,18 +5,17 @@ const USER = encodeURIComponent(process.env.DB_USER);
 const PASSWORD = encodeURIComponent(process.env.DB_PASSWORD);
 const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
 
-const sequelize = new Sequelize(URI,
-    {
-        dialect: 'postgres',
-        protocol: 'postgres',
-        logging: false,
-        dialectOptions: {
-          ssl: {
-            require: true,
-            rejectUnauthorized: false,
-          }
-        }
-    });
+const sequelize = new Sequelize(URI, {
+  dialect: 'postgres',
+  protocol: 'postgres',
+  logging: false,
+  dialectOptions: process.env.NODE_ENV === 'production' ? {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  } : {},
+});
 
 setupModels(sequelize);
 
