bb84ecc060e5b9155864ae0c3f6eda1a497aba2c
Walter Arce <142426140+devargmaster@users.noreply.github.com>
2024-09-18 18:16:50 -0300

    modificacion por problema de columna role
    si existe no lo agrega

--- db/migrations/20240917205026-role.js
+++ db/migrations/20240917205026-role.js
@@ -2,11 +2,19 @@
 const {UserSchema, USER_TABLE} = require('./../models/user.model');
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-   await queryInterface.addColumn(USER_TABLE,'role',UserSchema.role);
+  up: async (queryInterface, Sequelize) => {
+    // Verificar si la columna ya existe antes de intentar crearla
+    const tableDescription = await queryInterface.describeTable('users');
+    if (!tableDescription.role) {
+      await queryInterface.addColumn('users', 'role', {
+        type: Sequelize.STRING,
+        allowNull: false,
+        defaultValue: 'user',
+      });
+    }
   },
 
-  async down (queryInterface, Sequelize) {
-    await queryInterface.removeColumn(USER_TABLE,'role');
-  }
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.removeColumn('users', 'role');
+  },
 };
