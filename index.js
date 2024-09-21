a35f42392f65050f23980fd47f480964900c3d57
Walter Arce <142426140+devargmaster@users.noreply.github.com>
2024-09-21 13:20:02 -0300

    ajustes menores para que muestre por default a swagger
    ajustes menores para que muestre por default a swagger

--- index.js
+++ index.js
@@ -1,52 +1,37 @@
 import express from 'express';
-import cors from 'cors';
-import routerApi from './routes/index.js';
-import swaggerUi from'swagger-ui-express';
-import swaggerDocument from './swagger.js';
-import { logErrors, boomErrorHandler, errorHandler } from './middlewares/error.handler.js';
+import swaggerUi from 'swagger-ui-express';
+import swaggerJsdoc from 'swagger-jsdoc';
+import swaggerDocs from './swagger.js'; // Importa tu configuración de Swagger desde un archivo separado
 
 const app = express();
-const port = process.env.PORT || 3000;
 
-// Middleware para parsear JSON
-app.use(express.json());
-
-// Configuración de CORS
-const whitelist = [
-  'http://localhost:8000',
-  'https://hitflow.com.ar',
-  'https://whispering-brook-12889-24774d694fdb.herokuapp.com'
-];
-
-const corsOptions = {
-  origin: (origin, callback) => {
-    if (whitelist.includes(origin) || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error('No permitido por CORS'));
-    }
-  }
+// Configuración de Swagger
+const swaggerOptions = {
+  swaggerDefinition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'API Documentation',
+      version: '1.0.0',
+      description: 'API documentation for your application',
+    },
+    servers: [
+      {
+        url: 'http://localhost:3000',
+      },
+    ],
+  },
+  apis: ['./routes/*.js'], // Ruta a tus archivos de rutas
 };
 
-app.use(cors(corsOptions));
-// Ruta base simple
-app.get('/', (req, res) => {
+const swaggerSpec = swaggerJsdoc(swaggerOptions);
+app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+// Rutas de tu aplicación
+app.get('/api', (req, res) => {
   res.send('API is running On HitFlow Team 14 septiembre');
 });
 
-
-// Configurar Rutas
-routerApi(app);
-
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-// Middlewares de manejo de  errores
-app.use(logErrors);
-app.use(boomErrorHandler);
-app.use(errorHandler);
-
-swaggerDocument(app, port);
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-  console.log(`Swagger docs available at http://localhost:${port}/api-docs`);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
